8094cc48cffd5a29f2ae52913739041db5c9b472
Văn Tú <admin@MacBook-Air-cua-Ma.local>
2025-08-31 18:32:44 +0700

    commit

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ from starlette.middleware.cors import CORSMiddleware
 from app.admin import  setup_admin
 from app.database import Base, engine
 from app.services.firebase_service import initialize_firebase
-from app.routers import auth, user, common, tool, shipper, upload, notification, websocket, sender, conversations
+from app.routers import auth, user, common, shipper, upload, notification, websocket, sender, conversations
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -27,7 +27,6 @@ app.include_router(user.router, prefix="/user", tags=["user"])
 app.include_router(shipper.router, prefix="/shipper", tags=["shipper"])
 app.include_router(sender.router, prefix="/sender", tags=["sender"])
 app.include_router(common.router, prefix="/common", tags=["common"])
-app.include_router(tool.router, prefix="/t", tags=["tool"])
 app.include_router(conversations.router, prefix="/conversations", tags=["conversations"])
 app.include_router(upload.router, prefix="/upload", tags=["upload"])
 app.include_router(notification.router, prefix="/notification", tags=["notification"])
