1047c07fbf25afce22c3004f45aeab95c0d1c932
Văn Tú <admin@MacBook-Air-cua-Ma.local>
2025-11-07 18:28:21 +0700

    sửa lại tính năng quiz thay cho https://dlhub.vn/

--- app/routers/common.py
+++ app/routers/common.py
@@ -24,7 +24,7 @@ async def get_utilities(db: AsyncSession = Depends(get_db)):
     return build_response(
         status_code=200,
         detail=response_json(status=True, message="URL tiện ích", data={
-            "url": "https://dlhub.vn/"
+            "url": "https://dlhub.vn/articles/"
         })
     )
 
